cf1e307ee8bff88efe18ab708c8ca2b2b8345df4
Michael Perel <michaelsethperel@gmail.com>
2019-08-04 04:00:33 +0000

    wrote test ideas

--- generate/parse_test.go
+++ generate/parse_test.go
@@ -2,6 +2,7 @@ package generate
 
 import (
 	"os"
+	"path/filepath"
 	"sync"
 	"testing"
 )
@@ -20,23 +21,23 @@ func TestParseComposeFile(t *testing.T) {
 	os.Setenv("VERBOSE4CONTEXT", VERBOSE4CONTEXT)
 	os.Setenv("VERBOSE4DOCKERFILE", VERBOSE4DOCKERFILE)
 
-	baseDir := "testassets/parse/composefile/"
+	baseDir := filepath.Join("testassets", "parse", "composefile")
 
 	results := map[parsedImageLine]bool{
-		{line: "busybox", fileName: baseDir + "docker-compose.yml", err: nil}:           false,
-		{line: "busybox", fileName: baseDir + "simple2build/Dockerfile", err: nil}:      false,
-		{line: "busybox", fileName: baseDir + "simple3build/Dockerfile", err: nil}:      false,
-		{line: "busybox", fileName: baseDir + "simple4build/Dockerfile", err: nil}:      false,
-		{line: "busybox", fileName: baseDir + "verbose1build/Dockerfile", err: nil}:     false,
-		{line: "busybox", fileName: baseDir + "verbose2build/Dockerfile", err: nil}:     false,
-		{line: "busybox", fileName: baseDir + "verbose3build/Dockerfile", err: nil}:     false,
-		{line: "busybox", fileName: baseDir + "verbose4build/Dockerfile-dev", err: nil}: false,
+		{line: "busybox", fileName: filepath.Join(baseDir, "docker-compose.yml"), err: nil}:              false,
+		{line: "busybox", fileName: filepath.Join(baseDir, "simple2build", "Dockerfile"), err: nil}:      false,
+		{line: "busybox", fileName: filepath.Join(baseDir, "simple3build", "Dockerfile"), err: nil}:      false,
+		{line: "busybox", fileName: filepath.Join(baseDir, "simple4build", "Dockerfile"), err: nil}:      false,
+		{line: "busybox", fileName: filepath.Join(baseDir, "verbose1build", "Dockerfile"), err: nil}:     false,
+		{line: "busybox", fileName: filepath.Join(baseDir, "verbose2build", "Dockerfile"), err: nil}:     false,
+		{line: "busybox", fileName: filepath.Join(baseDir, "verbose3build", "Dockerfile"), err: nil}:     false,
+		{line: "busybox", fileName: filepath.Join(baseDir, "verbose4build", "Dockerfile-dev"), err: nil}: false,
 	}
 	parsedImageLines := make(chan parsedImageLine)
 	var wg sync.WaitGroup
 	go func() {
 		wg.Add(1)
-		parseComposefile(baseDir+"docker-compose.yml", parsedImageLines, &wg)
+		parseComposefile(filepath.Join(baseDir, "docker-compose.yml"), parsedImageLines, &wg)
 		wg.Wait()
 		close(parsedImageLines)
 	}()
@@ -62,4 +63,22 @@ func TestParseComposeFile(t *testing.T) {
 }
 
 func TestParseDockerfile(t *testing.T) {
+	baseDir := filepath.Join("testassets", "parse", "dockerfile")
+
+	// tests that compose build args overrides the ones defined in the dockerfile (simulate by passing in build vars)
+	dockerfile := filepath.Join(baseDir, "override", "Dockerfile")
+	//parseDockerfile(fileName string, buildArgs map[string]string, parsedImageLines chan<- parsedImageLine, wg *sync.WaitGroup)
+	//parseDockerfile
+	buildArgs := map[string]string{"IMAGE_NAME": "debian"}
+	_ = buildArgs
+	_ = dockerfile
+
+	// test that build vars reset on each new from line
+
+	// tests that ARG vars without values will be filled in by compose build args (similute by passing in build vars)
+
+	// tests that compose build args, which are not defined in the Dockerfile cannot be used
+	// have vars in docker-compose and try to use them in a Dockerfile where they are not defined.
+
+	// ENV in dockerfile overrides environment variables from compose files
 }
