f8ffc8eecb7ceddd7343501dca36952ba671223f
Michael Perel <michaelsethperel@gmail.com>
2019-08-23 14:57:33 -0400

    scaffolded compose

--- rewrite/rewriter.go
+++ rewrite/rewriter.go
@@ -7,11 +7,20 @@ import (
 	"io/ioutil"
 	"strings"
 	"sync"
+
+	"gopkg.in/yaml.v2"
 )
 
 type Rewriter struct {
 	generate.Lockfile
 	Postfix string
+}
+
+type compose struct {
+	Services map[string]struct {
+		Image string      `yaml:"image"`
+		Build interface{} `yaml:"build"`
+	} `yaml:"services"`
 }
 
 func NewRewriter(flags *Flags) (*Rewriter, error) {
@@ -29,13 +38,19 @@ func NewRewriter(flags *Flags) (*Rewriter, error) {
 // Rewrite rewrites base images to include their digests.
 // The order of rewrite is: Dockerfiles, Composefiles, Dockerfiles referenced by Composefiles.
 func (r *Rewriter) Rewrite() {
-	var wg sync.WaitGroup
-	for dpath, images := range r.DockerfileImages {
-		wg.Add(1)
-		go r.rewriteDockerfile(dpath, images, &wg)
+	// var dwg sync.WaitGroup
+	// for dpath, images := range r.DockerfileImages {
+	// 	dwg.Add(1)
+	// 	go r.rewriteDockerfile(dpath, images, &dwg)
+	// }
+	// dwg.Wait()
+
+    var cwg sync.WaitGroup
+	for cpath, images := range r.ComposefileImages {
+		cwg.Add(1)
+		r.rewriteComposefiles(cpath, images, &cwg)
 	}
-	wg.Wait()
-	r.rewriteComposefiles()
+	cwg.Wait()
 }
 
 func (r *Rewriter) rewriteDockerfile(dpath string, images []generate.DockerfileImage, wg *sync.WaitGroup) error {
@@ -75,6 +90,53 @@ func (r *Rewriter) rewriteDockerfile(dpath string, images []generate.DockerfileI
 	return nil
 }
 
-func (r *Rewriter) rewriteComposefiles() {
+func (r *Rewriter) rewriteComposefiles(cpath string, images []generate.ComposefileImage, wg *sync.WaitGroup) error {
+	if wg != nil {
+		defer wg.Done()
+	}
+	cByt, err := ioutil.ReadFile(cpath)
+	if err != nil {
+		return err
+	}
+	var comp compose
+	if err := yaml.Unmarshal(cByt, &comp); err != nil {
+		return err
+	}
+	// TODO: change docker-lock file so this step is unnecessary
+	lServices := make(map[string][]generate.ComposefileImage)
+	for _, image := range images {
+		if _, ok := lServices[image.ServiceName]; !ok {
+			lServices[image.ServiceName] = make([]generate.ComposefileImage, 0)
+		}
+		lServices[image.ServiceName] = append(lServices[image.ServiceName], image)
+	}
+	for serviceName, service := range comp.Services {
+		switch build := service.Build.(type) {
+		case map[interface{}]interface{}:
+			if build["context"] != nil || build["dockerfile"] != nil {
+				dFile, dImages := getDImageInfo(serviceName, lServices)
+				r.rewriteDockerfile(dFile, dImages, nil)
+			} else {
+				// record the service name, and the replacement image
+			}
+		case string:
+			dFile, dImages := getDImageInfo(serviceName, lServices)
+			r.rewriteDockerfile(dFile, dImages, nil)
+		default:
+			// record the service name, and the replacement image
+			fmt.Println(service, "does not exist")
+		}
+	}
 
+	// replace the service name with replacement image, rewrite the file.
+	return nil
+}
+
+func getDImageInfo(serviceName string, lServices map[string][]generate.ComposefileImage) (string, []generate.DockerfileImage) {
+	dImages := make([]generate.DockerfileImage, len(lServices[serviceName]))
+	dFile := lServices[serviceName][0].Dockerfile
+	for i, cImage := range lServices[serviceName] {
+		dImages[i] = generate.DockerfileImage{Image: cImage.Image}
+	}
+	return dFile, dImages
 }
