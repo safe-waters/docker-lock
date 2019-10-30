7d4a5ce1129ffb622ad9cbd7d4e0f5d5a93e768a
Michael Perel <miperel@microsoft.com>
2019-10-30 15:49:26 -0400

    got rid of dev null ...

--- integration_tests/integration_tests.sh
+++ integration_tests/integration_tests.sh
@@ -24,7 +24,7 @@
 
     run_integration_tests() {
         # docker logged out with no creds in .env, generate should fail
-        if ! docker lock verify --env-file .envwithoutcreds > /dev/null 2>&1; then
+        if ! docker lock verify --env-file .envwithoutcreds; then
             echo -e "\n ------ PASSED: docker lock failed after docker logout ------ \n"
         else
             echo -e "\n ------ ERROR: docker lock succeeded after docker logout ------ \n"
@@ -32,7 +32,7 @@
         fi
 
         # using .env but still logged out, generate should succeed
-        if docker lock verify --env-file .envwithcreds > /dev/null 2>&1; then
+        if docker lock verify --env-file .envwithcreds; then
             echo -e "\n ------ PASSED: docker lock succeeded after docker logout with .env credentials ------ \n"
         else
             echo -e "\n ------ ERROR: docker lock failed after docker logout with .env credentials ------ \n"
@@ -40,15 +40,15 @@
         fi
 
         # docker login again, generate should succeed
-        docker login $3 --username $1 --password $2 > /dev/null 2>&1
-        if docker lock verify --env-file .envwithoutcreds > /dev/null 2>&1; then
+        docker login $3 --username $1 --password $2
+        if docker lock verify --env-file .envwithoutcreds; then
             echo -e "\n ------ PASSED: docker lock succeeded after docker login again ------ \n"
         else
             echo -e "\n ------ ERROR: docker lock failed after docker login again ------ \n"
             exit 1
         fi
 
-        docker logout $3 > /dev/null 2>&1
+        docker logout $3
     }
 
     main() {
