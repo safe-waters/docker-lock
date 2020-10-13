e6989953d94268c380d707ad5bc29cf640937a7b
Michael Perel <michaelsethperel@gmail.com>
2020-10-12 23:34:48 -0400

    add rewrite

--- test/demo-app/tests.sh
+++ test/demo-app/tests.sh
@@ -6,7 +6,7 @@ function cleanup() {
     rm ./*-test.json
 }
 
-function compare_lockfiles() {
+function diff_files() {
     local lockfile
     local test_lockfile
 
@@ -18,36 +18,70 @@ function compare_lockfiles() {
     fi
 }
 
-trap cleanup EXIT
+function main() {
+    trap cleanup EXIT
 
-docker lock generate --lockfile-name docker-lock-test.json
-docker lock verify --lockfile-name docker-lock.json
-compare_lockfiles docker-lock.json docker-lock-test.json
+    echo "------ RUNNING GENERATE/VERIFY TESTS ------"
 
-docker lock generate --exclude-all-dockerfiles --lockfile-name docker-lock-exclude-all-dockerfiles-test.json
-docker lock verify --lockfile-name docker-lock-exclude-all-dockerfiles.json
-compare_lockfiles docker-lock-exclude-all-dockerfiles.json docker-lock-exclude-all-dockerfiles-test.json
+    echo "default"
+    docker lock generate --lockfile-name docker-lock-test.json
+    docker lock verify --lockfile-name docker-lock.json
+    diff_files docker-lock.json docker-lock-test.json
 
-docker lock generate --exclude-all-composefiles --lockfile-name docker-lock-exclude-all-composefiles-test.json
-docker lock verify --lockfile-name docker-lock-exclude-all-composefiles.json
-compare_lockfiles docker-lock-exclude-all-composefiles.json docker-lock-exclude-all-composefiles-test.json
+    echo "--exclude-all-dockerfiles"
+    docker lock generate --exclude-all-dockerfiles --lockfile-name docker-lock-exclude-all-dockerfiles-test.json
+    docker lock verify --lockfile-name docker-lock-exclude-all-dockerfiles.json
+    diff_files docker-lock-exclude-all-dockerfiles.json docker-lock-exclude-all-dockerfiles-test.json
 
-docker lock generate --lockfile-name docker-lock-base-dir-test.json
-docker lock verify --lockfile-name docker-lock-base-dir-test.json
-compare_lockfiles docker-lock-base-dir.json docker-lock-base-dir-test.json
+    echo "--exclude-all-composefiles"
+    docker lock generate --exclude-all-composefiles --lockfile-name docker-lock-exclude-all-composefiles-test.json
+    docker lock verify --lockfile-name docker-lock-exclude-all-composefiles.json
+    diff_files docker-lock-exclude-all-composefiles.json docker-lock-exclude-all-composefiles-test.json
 
-docker lock generate --dockerfiles web/Dockerfile --lockfile-name docker-lock-dockerfiles-test.json
-docker lock verify --lockfile-name docker-lock-dockerfiles-test.json
-compare_lockfiles docker-lock-dockerfiles.json docker-lock-dockerfiles-test.json
+    echo "--base-dir"
+    docker lock generate --lockfile-name docker-lock-base-dir-test.json
+    docker lock verify --lockfile-name docker-lock-base-dir-test.json
+    diff_files docker-lock-base-dir.json docker-lock-base-dir-test.json
 
-docker lock generate --composefiles docker-compose-1.yml --lockfile-name docker-lock-composefiles-test.json
-docker lock verify --lockfile-name docker-lock-composefiles-test.json
-compare_lockfiles docker-lock-composefiles.json docker-lock-composefiles-test.json
+    echo "--dockerfiles"
+    docker lock generate --dockerfiles web/Dockerfile --lockfile-name docker-lock-dockerfiles-test.json
+    docker lock verify --lockfile-name docker-lock-dockerfiles-test.json
+    diff_files docker-lock-dockerfiles.json docker-lock-dockerfiles-test.json
 
-docker lock generate --dockerfile-recursive --lockfile-name docker-lock-dockerfile-recursive-test.json
-docker lock verify --lockfile-name docker-lock-dockerfile-recursive-test.json
-compare_lockfiles docker-lock-dockerfile-recursive.json docker-lock-dockerfile-recursive-test.json 
+    echo "--composefiles"
+    docker lock generate --composefiles docker-compose.yml,docker-compose-1.yml --lockfile-name docker-lock-composefiles-test.json
+    docker lock verify --lockfile-name docker-lock-composefiles-test.json
+    diff_files docker-lock-composefiles.json docker-lock-composefiles-test.json
 
-docker lock generate --composefile-recursive --lockfile-name docker-lock-composefile-recursive-test.json
-docker lock verify --lockfile-name docker-lock-composefile-recursive-test.json
-compare_lockfiles docker-lock-composefile-recursive.json docker-lock-composefile-recursive-test.json
+    echo "--dockerfile-recursive"
+    docker lock generate --dockerfile-recursive --lockfile-name docker-lock-dockerfile-recursive-test.json
+    docker lock verify --lockfile-name docker-lock-dockerfile-recursive-test.json
+    diff_files docker-lock-dockerfile-recursive.json docker-lock-dockerfile-recursive-test.json 
+
+    echo "--composefile-recursive"
+    docker lock generate --composefile-recursive --lockfile-name docker-lock-composefile-recursive-test.json
+    docker lock verify --lockfile-name docker-lock-composefile-recursive-test.json
+    diff_files docker-lock-composefile-recursive.json docker-lock-composefile-recursive-test.json
+
+    echo "--dockerfile-globs"
+    docker lock generate --dockerfile-globs 'web/*','database/*' --lockfile-name docker-lock-dockerfile-globs-test.json 
+    docker lock verify --lockfile-name docker-lock-dockerfile-globs-test.json
+    diff_files docker-lock-dockerfile-globs.json docker-lock-dockerfile-globs-test.json
+
+    echo "--composefile-globs"
+    docker lock generate --composefile-globs 'docker-compose*.yml' --lockfile-name docker-lock-composefile-globs-test.json 
+    docker lock verify --lockfile-name docker-lock-composefile-globs-test.json
+    diff_files docker-lock-composefile-globs.json docker-lock-composefile-globs-test.json
+
+    echo "------ PASSED GENERATE/VERIFY TESTS ------"
+
+    echo "------ RUNNING REWRITE TESTS ------"
+
+    echo "default"
+    docker lock rewrite
+    diff_files docker-compose.yml docker-compose-rewrite.yml
+    diff_files web/Dockerfile web/Dockerfile-rewrite
+    diff_files database/Dockerfile database/Dockerfile-rewrite
+
+    echo "------ PASSED REWRITE TESTS ------"
+}
