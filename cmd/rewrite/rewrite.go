f7c3393e3e306d09cd96e4aced98163a3f5a9d49
Michael Perel <michaelsethperel@gmail.com>
2020-10-18 19:23:28 -0400

    namespace rewrite vars

--- cmd/rewrite/rewrite.go
+++ cmd/rewrite/rewrite.go
@@ -2,6 +2,7 @@
 package rewrite
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/safe-waters/docker-lock/pkg/rewrite"
@@ -10,13 +11,19 @@ import (
 	"github.com/spf13/viper"
 )
 
+const namespace = "rewrite"
+
 // NewRewriteCmd creates the command 'rewrite' used in 'docker lock rewrite'.
 func NewRewriteCmd() (*cobra.Command, error) {
 	rewriteCmd := &cobra.Command{
 		Use:   "rewrite",
 		Short: "Rewrite files referenced by a Lockfile to use image digests",
 		PreRunE: func(cmd *cobra.Command, args []string) error {
-			return viper.BindPFlags(cmd.Flags())
+			return bindPFlags(cmd, []string{
+				"lockfile-name",
+				"tempdir",
+				"exclude-tags",
+			})
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			flags, err := parseFlags()
@@ -76,12 +83,30 @@ func SetupRewriter(flags *Flags) (*rewrite.Rewriter, error) {
 	return rewrite.NewRewriter(writer, renamer)
 }
 
+func bindPFlags(cmd *cobra.Command, flagNames []string) error {
+	for _, name := range flagNames {
+		if err := viper.BindPFlag(
+			fmt.Sprintf("%s.%s", namespace, name), cmd.Flags().Lookup(name),
+		); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 // parseFlags gets values from the command and uses them to
 // create Flags.
 func parseFlags() (*Flags, error) {
-	lockfileName := viper.GetString("lockfile-name")
-	tempDir := viper.GetString("tempdir")
-	excludeTags := viper.GetBool("exclude-tags")
+	lockfileName := viper.GetString(
+		fmt.Sprintf("%s.%s", namespace, "lockfile-name"),
+	)
+	tempDir := viper.GetString(
+		fmt.Sprintf("%s.%s", namespace, "tempdir"),
+	)
+	excludeTags := viper.GetBool(
+		fmt.Sprintf("%s.%s", namespace, "exclude-tags"),
+	)
 
 	return NewFlags(lockfileName, tempDir, excludeTags)
 }
